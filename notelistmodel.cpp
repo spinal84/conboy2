0b80b8ba0b0ad5331fb266112a25f6ed3091d433
Cornelius Hald <cornelius.hald@tourschall.com>
2011-07-17 19:42:29 +0200

    Implemented basic list model functionality

--- notelistmodel.cpp
+++ notelistmodel.cpp
@@ -3,4 +3,64 @@
 NoteListModel::NoteListModel(QObject *parent) :
     QAbstractListModel(parent)
 {
+    QHash<int, QByteArray> roles;
+    roles[UuidRole] = "uuid";
+    roles[TitleRole] = "title";
+    roles[LastChangeDateRole] = "lastChangeDate";
+    setRoleNames(roles);
 }
+
+int NoteListModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return notes.count();
+}
+
+QVariant NoteListModel::data(const QModelIndex &index, int role) const
+{
+    if (index.row() < 0 || index.row() > notes.count()) {
+        return QVariant();
+    }
+
+    NoteData *note = notes[index.row()];
+
+    switch (role) {
+    case UuidRole: return note->getUuid().toString();
+    case TitleRole: return note->getTitle();
+    case LastChangeDateRole: return note->getLastChangeDate();
+    default: return QVariant();
+    }
+}
+
+bool compareTitle(const NoteData *n1, const NoteData *n2)
+{
+    if (n1 == 0 || n2 == 0) {
+        return false;
+    }
+
+    return n1->getTitle() < n2->getTitle();
+}
+
+bool compareLastChangeDate(const NoteData *n1, const NoteData *n2)
+{
+    if (n1 == 0 || n2 == 0) {
+        return false;
+    }
+
+    return n1->getLastChangeDate() < n2->getLastChangeDate();
+}
+
+// column=0 -> sort by title. column=1 -> sort by last change date
+void NoteListModel::sort(int column, Qt::SortOrder order)
+{
+    Q_UNUSED(order);
+    emit layoutAboutToBeChanged();
+    if (column == 0) {
+        qStableSort(notes.begin(), notes.end(), compareTitle);
+    } else if (column == 1) {
+        qStableSort(notes.begin(), notes.end(), compareLastChangeDate);
+    }
+    emit layoutChanged();
+}
+
+
--- notelistmodel.h
+++ notelistmodel.h
@@ -3,6 +3,8 @@
 
 #include <QAbstractListModel>
 
+#include "notedata.h"
+
 /*
   A list that can be sorted and filtered. The content of this list
   will be displayed to the user through QML.
@@ -18,13 +20,27 @@ class NoteListModel : public QAbstractListModel
 {
     Q_OBJECT
 public:
+
+    enum NoteRoles {
+        UuidRole = Qt::UserRole + 100,
+        TitleRole,
+        LastChangeDateRole
+    };
+
     explicit NoteListModel(QObject *parent = 0);
 
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+    void sort(int column, Qt::SortOrder order);
+    void append(NoteData *note);
 
 
 signals:
 
 public slots:
+
+private:
+    QList<NoteData*> notes;
 
 };
 
